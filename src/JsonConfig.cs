b149813f6047fa309802c587378b593649117a52
Michael Easter <codetojoy@gmail.com>
2021-02-23 09:42:30 -0400

    incremental update Tue 23 Feb 2021 09:42:30 AST

---
+++ src/JsonConfig.cs
@@ -0,0 +1,58 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace WarO_CSharp_v2
+{
+    public class JsonConfig : IConfig
+    {
+        private static bool isVerbose = true;
+        private int maxCard = 16;
+
+        public JsonConfig(string jsonFile)
+        {
+
+        }
+
+        public bool IsVerbose()
+        {
+            return isVerbose;
+        }
+
+        public int GetNumCardsPerHand()
+        {
+            int numPlayers = GetPlayers().Count;
+            int numCardsPerHand = maxCard / (numPlayers + 1);
+            return numCardsPerHand;
+        }
+
+        public int GetMaxCard()
+        {
+            return maxCard;
+        }
+        public List<Player> GetPlayers()
+        {
+            List<Player> players = new List<Player>();
+
+            Player p1 = new Player("You", new ConsoleStrategy(), GetMaxCard());
+            Player p2 = new Player("Chopin", new NextCard(), GetMaxCard());
+            Player p3 = new Player("Mozart", new NextCard(), GetMaxCard());
+
+            players.Add(p1);
+            players.Add(p2);
+            players.Add(p3);
+
+            return players;
+        }
+
+        public override string ToString()
+        {
+            string result = "\n";
+            result += "maxCard: " + maxCard + "\n";
+            foreach (Player player in GetPlayers()) {
+                result += player.ToString() + "\n";
+            }
+            return result;
+        }
+    }
+}
